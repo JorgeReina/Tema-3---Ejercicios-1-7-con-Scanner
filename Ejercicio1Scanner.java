f37122b21a2eceb0de23c66138be03e4c2367082
Jorge Reina Romero <116582548+JorgeReina@users.noreply.github.com>
2022-11-13 18:44:06 +0100

    Add files via upload

--- Ejercicio1Scanner.java
+++ Ejercicio1Scanner.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 /**
 *
-*	Ejercicio 1, Pide dos numeros por teclado y muestra el resultado de su multiplicación.
+*	Ejercicio 1, Pide dos numeros por teclado y muestra el resultado de su multiplicación,
+* 	utilizando la clase Scanner.
 * 
 *	Nombre del archivo: Ejercicio1.java
 *
@@ -12,14 +13,18 @@ import java.util.Scanner;
 
 public class Ejercicio1Scanner {
 	public static void main(String[] args) {
+		
+		//Definimos la clase.
 		Scanner s = new Scanner(System.in);
 		
+		//Utilizamos "s.nexInt()" para leer un número entero.
 		System.out.print("Por favor, introduce un número: ");
 		int primerNumero = s.nextInt();
 		
 		System.out.print("Introduce el segundo número: ");
 		int segundoNumero = s.nextInt();
 		
+		//Variable que calcula la operación.
 		int mult = primerNumero * segundoNumero;
 		
 		System.out.println("El resultado de '" + primerNumero + "' multiplicado por '" + segundoNumero + "' es: " + mult);	
